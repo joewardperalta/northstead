baaa0a2f77fa5ed9128aec7f3552c6725d5cb390
Joeward Peralta <joeward_peralta@outlook.com>
2025-10-13 19:05:19 -0400

    Create a contact form

---
+++ app/contact/page.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+// Components
+import Heading from "@/components/Heading";
+import Headline from "@/components/Headline";
+import Section from "@/components/Section";
+import SubHeading from "@/components/SubHeading";
+import SubTitle from "@/components/SubTitle";
+import Tagline from "@/components/Tagline";
+import Title from "@/components/Title";
+import Wrapper from "@/components/Wrapper";
+import Form from "next/form";
+import { useState } from "react";
+
+export default function Home() {
+  const [phone, setPhone] = useState("");
+  const [error, setError] = useState("");
+
+  function handlePhoneChange(e: React.ChangeEvent<HTMLInputElement>) {
+    // Remove all non-digit and non-plus characters
+    const value = e.target.value.replace(/[^0-9]/g, "");
+
+    setPhone(value);
+
+    const phoneRegex = /^[0-9]{10,15}$/;
+    if (!phoneRegex.test(value)) {
+      setError("Please enter a valid phone number.");
+    } else {
+      setError("");
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!error) {
+      const formData = new FormData(e.currentTarget);
+
+      // Get individual values
+      const firstname = formData.get("firstname");
+      const lastname = formData.get("lastname");
+      const email = formData.get("email");
+      const phone = formData.get("phone");
+      const message = formData.get("message");
+    }
+  };
+
+  return (
+    <>
+      {/* Hero Section */}
+      <Section className="bg-[url(/photos/professional-team-smiling.jpg)] bg-cover bg-center">
+        <Wrapper>
+          <Headline className="text-white pt-[6rem] mb-0">
+            <Title>Contact us</Title>
+            <SubTitle>
+              Connect with us today and take the first step toward your Canadian
+              journey with confidence. Our team at Northstead Immigration Inc.
+              is ready to provide the guidance and support you need every step
+              of the way.
+            </SubTitle>
+          </Headline>
+        </Wrapper>
+      </Section>
+
+      {/* Contact form */}
+      <Section>
+        <Wrapper>
+          <Headline className="mb-0">
+            <Tagline isDarkTheme>How we help clients</Tagline>
+            <Heading>Need help with immigration or Visa applications?</Heading>
+            <SubHeading>
+              We are always ready to support our clients and provide solutions
+              to any challenges they face during their immigration journey.
+            </SubHeading>
+          </Headline>
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <input
+                className="input"
+                type="text"
+                name="firstname"
+                id="firstname"
+                placeholder="First name"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                placeholder="Last name"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email address"
+                required
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                name="phone"
+                id="phone"
+                placeholder="Phone number"
+                pattern="[0-9]*"
+                inputMode="numeric"
+                value={phone}
+                onChange={handlePhoneChange}
+                required
+                className={`${error ? "border-red-500" : ""}`}
+              />
+              {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+            </div>
+            <div>
+              <textarea
+                placeholder="Tell us about your case"
+                rows={10}
+                name="message"
+                id="message"
+                required
+              />
+            </div>
+            <button className="btn primary-btn" type="submit">
+              Send message
+            </button>
+          </form>
+        </Wrapper>
+      </Section>
+    </>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,7 @@ export default function Navbar() {
     { label: "Home", link: "/" },
     { label: "About", link: "/about" },
     { label: "Services", link: "/services" },
+    { label: "Contact", link: "/contact" },
   ];
 
   function handleHamburgerButtonClick() {
