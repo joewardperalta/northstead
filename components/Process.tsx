edc2bc765339e2b19ea120d0ff277fc9afc1aa68
Joeward Peralta <joeward_peralta@outlook.com>
2025-10-14 13:03:31 -0400

    Update the process section and set it to have 2-column when viewed in tablet and 3-column grid when viewed in desktop

--- components/Process.tsx
+++ components/Process.tsx
@@ -11,23 +11,22 @@ import steps from "@/data/process.json";
 export default function Process() {
   return (
     <Section className="bg-primary text-white">
-      <Wrapper>
+      <Wrapper className="md:grid md:grid-cols-2 lg:grid-cols-3 md:space-y-0">
         <Headline>
           <Tagline>Step by step process</Tagline>
           <Heading>We make immigration simple</Heading>
         </Headline>
 
-        <div className="flex flex-col gap-4">
-          {steps.map((step, index) => (
+        {steps.map((step, index) => (
+          <article key={index}>
             <StepCard
-              key={index}
               title={step.title}
               description={step.description}
               backgroundImg={step.image}
               step={index + 1}
             />
-          ))}
-        </div>
+          </article>
+        ))}
       </Wrapper>
     </Section>
   );
