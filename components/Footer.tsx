ecebf0ef244b1a7da1f1840257ef9f4c0d0e1af4
Joeward Peralta <joeward_peralta@outlook.com>
2025-10-14 13:48:16 -0400

    Change the layout of footer component when viewed in tablet and desktop device

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,84 +14,88 @@ const pages = [
 export default function Footer() {
   return (
     <footer className="py-14">
-      <Wrapper className="space-y-12">
-        {/* Company */}
-        <div className="space-y-3">
-          <Logo />
-          <p className="text-lg">
-            At Northstead Immigration, we believe every journey deserves
-            clarity, care, and confidence as we guide you through each step to
-            turn your dream of a new life in Canada into reality.
-          </p>
-        </div>
+      <Wrapper className="space-y-12 md:space-y-24">
+        <div className="space-y-12 md:space-y-0 md:flex">
+          {/* Company */}
+          <div className="space-y-3 md:pr-[10rem] md:max-w-[34rem]">
+            <Logo />
+            <p className="text-lg">
+              At Northstead Immigration, we believe every journey deserves
+              clarity, care, and confidence as we guide you through each step to
+              turn your dream of a new life in Canada into reality.
+            </p>
+          </div>
 
-        {/* Pages */}
-        <div>
-          <TertiaryHeading className="text-base font-bold mb-5">
-            Pages
-          </TertiaryHeading>
-          <ul className="space-y-4">
-            {pages.map((page, index) => (
-              <li key={index}>
-                <Link href={page.link}>{page.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+          <div className="space-y-8 md:space-y-0 md:flex md:w-full md:justify-end md:gap-30">
+            {/* Pages */}
+            <div className="">
+              <TertiaryHeading className="text-base font-bold mb-5">
+                Pages
+              </TertiaryHeading>
+              <ul className="space-y-4">
+                {pages.map((page, index) => (
+                  <li key={index}>
+                    <Link href={page.link}>{page.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-        {/* Contact information */}
-        <div>
-          <TertiaryHeading className="text-base font-bold mb-5">
-            Contact information
-          </TertiaryHeading>
-          <ul className="space-y-4">
-            <li className="flex gap-3 items-center">
-              <Image
-                className="w-4 h-4"
-                src="/icons/map.png"
-                alt=""
-                width={64}
-                height={64}
-              />
-              <p>541 Laval St, Oshawa, ON L1J 6L8</p>
-            </li>
-            <li className="flex gap-3 items-center">
-              <Image
-                className="w-4 h-4"
-                src="/icons/envelope.png"
-                alt=""
-                width={64}
-                height={64}
-              />
-              <Link href="mailto:admin@northsteadimmig.com">
-                admin@northsteadimmig.com
-              </Link>
-            </li>
-            <li className="flex gap-3 items-center">
-              <Image
-                className="w-4 h-4"
-                src="/icons/phone.png"
-                alt=""
-                width={64}
-                height={64}
-              />
-              <Link href="tel:+16472704116">+1 (647) 270-4116</Link>
-            </li>
-            <li className="flex gap-3 items-center">
-              <Image
-                className="w-4 h-4"
-                src="/icons/clock.png"
-                alt=""
-                width={64}
-                height={64}
-              />
-              <p>Mon - Sat: 9am-5pm</p>
-            </li>
-          </ul>
+            {/* Contact information */}
+            <div className="">
+              <TertiaryHeading className="text-base font-bold mb-5">
+                Contact information
+              </TertiaryHeading>
+              <ul className="space-y-4">
+                <li className="flex gap-3 items-center">
+                  <Image
+                    className="w-4 h-4"
+                    src="/icons/map.png"
+                    alt=""
+                    width={64}
+                    height={64}
+                  />
+                  <p>541 Laval St, Oshawa, ON L1J 6L8</p>
+                </li>
+                <li className="flex gap-3 items-center">
+                  <Image
+                    className="w-4 h-4"
+                    src="/icons/envelope.png"
+                    alt=""
+                    width={64}
+                    height={64}
+                  />
+                  <Link href="mailto:admin@northsteadimmig.com">
+                    admin@northsteadimmig.com
+                  </Link>
+                </li>
+                <li className="flex gap-3 items-center">
+                  <Image
+                    className="w-4 h-4"
+                    src="/icons/phone.png"
+                    alt=""
+                    width={64}
+                    height={64}
+                  />
+                  <Link href="tel:+16472704116">+1 (647) 270-4116</Link>
+                </li>
+                <li className="flex gap-3 items-center">
+                  <Image
+                    className="w-4 h-4"
+                    src="/icons/clock.png"
+                    alt=""
+                    width={64}
+                    height={64}
+                  />
+                  <p>Mon - Sat: 9am-5pm</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
 
         {/* Copyright */}
-        <div className="space-y-3 ">
+        <div className="space-y-3 md:space-y-0 md:flex md:justify-between md:items-center">
           <p className="text-sm">
             © 2025 Northstead Immigration Inc. All rights reserved.
           </p>
