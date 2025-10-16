b56c0e085dddbb8160d2b29228b61f7519661b9a
Joeward Peralta <joeward_peralta@outlook.com>
2025-10-16 12:37:35 -0400

    Update the Service card componet to remove the button and clickable card

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 type ServiceCardProps = {
   title: string;
   description: string;
-  link: string;
   backgroundImg: string;
   className?: string;
 };
@@ -13,30 +12,26 @@ type ServiceCardProps = {
 export default function ServiceCard({
   title,
   description,
-  link,
   backgroundImg,
   className,
 }: ServiceCardProps) {
   return (
-    <Link href={link}>
-      <div
-        className={`service-card bg-cover relative ${className}`}
-        style={{ backgroundImage: `url(/photos/${backgroundImg})` }}
-      >
-        {/* Overlay */}
-        <div className="bg-black/50 absolute inset-0"></div>
+    <section
+      className={`service-card bg-cover relative ${className}`}
+      style={{ backgroundImage: `url(/photos/${backgroundImg})` }}
+    >
+      {/* Overlay */}
+      <div className="bg-black/50 absolute inset-0"></div>
 
-        {/* Content */}
-        <div className="relative z-10 flex flex-col justify-between">
-          <TertiaryHeading className="mb-[12rem]">{title}</TertiaryHeading>
+      {/* Content */}
+      <div className="relative z-10 flex flex-col justify-between">
+        <TertiaryHeading className="mb-[12rem]">{title}</TertiaryHeading>
 
-          {/* Description */}
-          <div className="md:min-h-[14rem] md:flex md:flex-col md:justify-end">
-            <p className="text-base leading-[1.4] mb-8">{description}</p>
-            <button className="btn cursor-pointer">Learn more</button>
-          </div>
+        {/* Description */}
+        <div className="md:min-h-[14rem] md:flex md:flex-col md:justify-end">
+          <p className="text-base leading-[1.4] mb-8">{description}</p>
         </div>
       </div>
-    </Link>
+    </section>
   );
 }
