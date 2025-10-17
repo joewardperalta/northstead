2dd178cff2ef54122707e74e7e6360cac3f5404f
Joeward Peralta <joeward_peralta@outlook.com>
2025-10-17 01:17:58 -0400

    Add web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.className} antialiased`}>
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
